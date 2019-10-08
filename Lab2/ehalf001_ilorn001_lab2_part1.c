002f4c9c8b35705094ed3a19b4bbe4ac9bf121dd
ehalf001 <38255772+ehalf001@users.noreply.github.com>
2019-10-08 02:26:55 -0700

    Naming

--- Lab2/ehalf001_ilorn001_lab2_part1.c
+++ Lab2/ehalf001_ilorn001_lab2_part1.c
@@ -1,7 +1,7 @@
 /*    Name & Email: Ivan Lorna / ilorn001@ucr.edu
  *    Partner(s) Name & E-mail: Emanuel Halfon / ehalf001@ucr.edu
  *    Lab Section:  021
- *    Assignment: Lab 2
+ *    Assignment: Lab 2 Part 1
  *    Exercise Description: [optional - include for your own benefit]
  *    
  *    I acknowledge all content contained herein, excluding template 
@@ -54,7 +54,7 @@ void TimerOff() {
 
 
 typedef struct Task {
-	int state; // Task’s current state
+	int state; // Taskâ€™s current state
 	unsigned long period; // Task period
 	unsigned long elapsedTime; // Time elapsed since last task tick
 	int (*TickFct)(int); // Task tick function
