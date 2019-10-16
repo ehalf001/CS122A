335fab57710c01b28b41f02f961158d48196b67f
ehalf001 <38255772+ehalf001@users.noreply.github.com>
2019-10-16 13:10:03 -0700

    Rename ehalf001_ilorn001_lab3_part3_Slave.c to Lab3/ehalf001_ilorn001_lab3_part3_Slave.c

--- Lab3/ehalf001_ilorn001_lab3_part3_Slave.c
+++ Lab3/ehalf001_ilorn001_lab3_part3_Slave.c
@@ -54,7 +54,7 @@ void TimerOff() {
 
 
 typedef struct Task {
-	int state; // Task’s current state
+	int state; // Taskâ€™s current state
 	unsigned long period; // Task period
 	unsigned long elapsedTime; // Time elapsed since last task tick
 	int (*TickFct)(int); // Task tick function
