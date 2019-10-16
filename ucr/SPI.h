d6bbbb2706c3a4f3b8490ee699edb6c3c14763e4
ehalf001 <38255772+ehalf001@users.noreply.github.com>
2019-10-16 13:09:13 -0700

    SPI header

---
+++ ucr/SPI.h
@@ -0,0 +1,37 @@
+#ifndef __SPI_h__
+#define __SPI_h__
+
+void SPI_MasterInit(void)
+{
+	/* Set MOSI and SCK output, all others input */
+	DDRB = (1<<DDRB4)|(1<<DDRB5)|(1<<DDRB7);
+	/* Enable SPI, Master, set clock rate fck/16 */
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+}
+
+
+void SPI_MasterTransmit(char cData)
+{
+	/* Start transmission */
+	SPDR = cData;
+	/* Wait for transmission complete */
+	while(!(SPSR & (1<<SPIF)));
+}
+
+void SPI_SlaveInit(void)
+{
+	/* Set MISO output, all others input */
+	PORTB = (1<<PORTB4)|(1<<PORTB6);
+	/* Enable SPI */
+	SPCR = (1<<SPE);
+}
+char SPI_SlaveReceive(void)
+{
+	/* Wait for reception complete */
+	while(!(SPSR & (1<<SPIF)));
+	/* Return Data Register */
+	return SPDR;
+}
+
+#endif
+
