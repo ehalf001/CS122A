19a8542f13d4791d84d5b174379823d06e8503ff
ehalf001 <38255772+ehalf001@users.noreply.github.com>
2019-11-22 14:09:00 -0800

    Update Version 1.1.0.c

--- Final Project/Version 1.1.0.c
+++ Final Project/Version 1.1.0.c
@@ -1,12 +1,4 @@
-/*    Name & Email: Ivan Lorna / ilorn001@ucr.edu
- *    Partner(s) Name & E-mail: Emanuel Halfon / ehalf001@ucr.edu
- *    Lab Section:  021
- *    Assignment: Lab 2 Part 1
- *    Exercise Description: [optional - include for your own benefit]
- *    
- *    I acknowledge all content contained herein, excluding template 
- *     or example code, is my own original work.
- */
+//Milestone 1
 
 //Includes
 #include <avr/io.h>
