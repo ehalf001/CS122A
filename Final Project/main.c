550dd435c2f7cb01ebfcaffa1a8586c11189db03
ehalf001 <38255772+ehalf001@users.noreply.github.com>
2019-11-25 10:38:48 -0800

    Update main.c

--- Final Project/main.c
+++ Final Project/main.c
@@ -4,37 +4,7 @@
 #include "usart_ATmega1284.h"
 
 #define B1 (~PINB & 0x01)
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------
 
-//taken from http://extremeelectronics.co.in/avr-tutorials/using-adc-of-avr-microcontroller/
-// void ADC_init()
-// {
-// 	ADMUX=(1<<REFS1)|(1<<REFS0);                         // For Aref=AVcc;
-// 	ADCSRA=(1<<ADEN)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); //Rrescalar div factor =128
-// }
-// uint16_t ReadADC(uint8_t ch){//I added the small delays
-// 	//Select ADC Channel ch must be 0-7
-// 	ch=ch&0b00000111;
-// 	ADMUX|=ch;
-// 	_delay_ms(1);
-// 	
-// 	//Start Single conversion
-// 	ADCSRA|=(1<<ADSC);
-// 	_delay_ms(1);
-// 	
-// 	//Wait for conversion to complete
-// 	while(!(ADCSRA & (1<<ADIF)));
-// 	//Clear ADIF by writing one to it
-// 	//Note you may be wondering why we have write one to clear it
-// 	//This is standard way of clearing bits in io as said in datasheets.
-// 	//The code writes '1' but it result in setting bit to '0' !!!
-// 	ADCSRA|=(1<<ADIF);
-// 	return(ADC);
-// }
-
-//---------------------------------------------------------------------------
-//---------------------------------------------------------------------------
 int main(){
 	DDRA = 0xFF; PORTA = 0x00;
 	DDRB = 0x00; PORTB = 0xFF;
